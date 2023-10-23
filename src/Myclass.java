0b4359ff3fb7b17c6e7b2d937284908b7c8a6497
razanafaneh <razanafaneh01@gmail.com>
2023-10-23 13:17:36 +0300

    update

---
+++ src/Myclass.java
@@ -0,0 +1,35 @@
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class Myclass {
+	WebDriver driver =new ChromeDriver();
+	
+	
+	
+	@BeforeTest 
+	
+	public void myBeforTest () {
+		driver.get("https://www.saucedemo.com/");
+		driver.findElement(By.xpath("//*[@id=\"user-name\"]")).sendKeys("standard_user");	
+		driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("secret_sauce");
+		driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+		List<WebElement> addtoCartButtons = driver.findElements(By.className("btn_primary"));
+		for(int i = 0 ;i<addtoCartButtons.size();i=i+2) {
+			addtoCartButtons.get(i).click();
+			
+		}
+}
+	@Test ()
+	public void myTestOne () {}
+	
+	@AfterTest
+	public void myAfterTest() {
+	}
+}
